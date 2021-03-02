ee974e8053d2ac2782cd311ad43ad5a638a7d39e
MICHAEL KINGS <kmichael381@gmail.com>
2021-03-02 13:19:00 -0800

    for-loops-js

---
+++ script.js
@@ -0,0 +1,30 @@
+console.log('connected');
+  
+// for loop
+
+for (let i = 0; i < 5; i++) {
+   console.log('Package' + i);
+   if(i ===3) break;
+    
+}
+
+const names = ['John','Peter','Jane','Cassy']
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+    
+}
+
+
+for (let i = 5; i > 0; i--) {
+    console.log('car' + i);
+     
+ }
+
+//  for of loop
+const items = ['books','Pens','Bags','Pencils']
+for ( item of items) {
+    console.log(items);
+    
+}
+ 
+ 
